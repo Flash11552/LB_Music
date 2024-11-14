4c21fa020dee2201bb8de3432938c5caa288a53a
Flash11552 <sfrovli7@gmail.com>
2024-11-15 03:02:03 +0400

    Update config.py

--- config.py
+++ config.py
@@ -8,22 +8,22 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID", None))
-API_HASH = getenv("API_HASH", None)
+API_ID = int(getenv("API_ID", 20411992))
+API_HASH = getenv("API_HASH", 21046ad069cc67f99ab5bb5de8e0b664)
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN", None)
+BOT_TOKEN = getenv("BOT_TOKEN", 8092836767:AAEzeZY7aE3W55hW6HIkJSHNVMUyynFF-Mk)
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_URI = getenv("MONGO_DB_URI", mongodb+srv://BrandedSupportGroup:BRANDED_WORLD@cluster0.v4odcq9.mongodb.net/?retryWrites=true&w=majority)
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", None)
 PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", None)
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", None))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", None))
+LOGGER_ID = int(getenv("LOGGER_ID", 5967971901))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", -1002447671873))
 
 # Get this value Telegram id
 OWNER_ID = int(getenv("OWNER_ID", "5016109398"))
